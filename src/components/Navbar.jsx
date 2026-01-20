1ecda28e491c9ded09ed8cccc25051ef6af0381f
baz2024 <basel.magableh@dbs.ie>
2026-01-20 18:48:57 +0000

    add avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = ({ onMenuClick }) => {
   const username = localStorage.getItem('user') || 'User';
+  const avatarurl = "https://i.pravatar.cc/300?u=" + username;
   return (
     <AppBar position="static">
       <Toolbar>
@@ -12,7 +13,7 @@ const Navbar = ({ onMenuClick }) => {
         </IconButton>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>Dashboard</Typography>
         <Tooltip title={username}>
-          <Avatar>{username.charAt(0).toUpperCase()}</Avatar>
+          <Avatar src={avatarurl} alt={username} />
         </Tooltip>
       </Toolbar>
     </AppBar>
